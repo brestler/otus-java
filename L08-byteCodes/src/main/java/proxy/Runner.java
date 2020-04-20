75a07dc12f9a9d3ba7ef24d25dd78b886cd589d4
brestler <devgeorgiiermakov@gmail.com>
2020-04-20 19:26:15 +0300

    Revert "Removed constructor argument"
    This reverts commit c525e6ad

--- L08-byteCodes/src/main/java/proxy/Runner.java
+++ L08-byteCodes/src/main/java/proxy/Runner.java
@@ -3,12 +3,12 @@ package proxy;
 public class Runner {
 
     public static void main(String[] args) {
-        IoC<Game> proxyMaker = new IoC<>();
+        IoC<Game> proxyMaker = new IoC<>(Game.class);
         Game game = proxyMaker.createProxy(new MyGame());
         game.play();
         game.play("Ho-ho");
 
-        IoC<NotAGameAtAll> anotherProxyMaker = new IoC<>();
+        IoC<NotAGameAtAll> anotherProxyMaker = new IoC<>(NotAGameAtAll.class);
         NotAGameAtAll notAGameAtAll = anotherProxyMaker.createProxy(new MyNotAGameAtAll());
         notAGameAtAll.work();
     }
